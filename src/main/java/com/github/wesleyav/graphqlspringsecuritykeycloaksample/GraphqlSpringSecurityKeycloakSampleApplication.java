53a461ec7b4fd4f37e4c00175222fac59e98fa43
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2022-02-17 18:22:53 -0300

    Adiciona estrutura do projeto

---
+++ src/main/java/com/github/wesleyav/graphqlspringsecuritykeycloaksample/GraphqlSpringSecurityKeycloakSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.graphqlspringsecuritykeycloaksample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringSecurityKeycloakSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringSecurityKeycloakSampleApplication.class, args);
+	}
+
+}
